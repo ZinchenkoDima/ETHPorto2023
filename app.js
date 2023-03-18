cd93bd80f80531b3d638e8b38add0ded98793b39
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-18 03:05:41 +0000

    att connection metamask

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-
 const ethereumButton = document.querySelector('.connect-metamask');
 
-// const web3 = new Web3('https://mainnet.infura.io/v3/3a17ed6d5b1f419fbfca7ff8328c0268');
 const web3 = new Web3(window.ethereum);
-
 
 web3.eth.net.isListening()
   .then(() => console.log('Thank you'))
   .catch((err) => console.log('Error:', err));
 
+const accountElem = document.querySelector('#account-address'); // seleciona o elemento pelo ID "account-address"
+let accounts;
+let account;
 
-const accountElem = document.querySelector("#account")
-let accounts
-let account 
-
-;(async () =>  {
+async function connectMetamask() {
   accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
   account = accounts[0];
-  console.log('your address:', account);
-  accountElem.innerHTML = account
-})()
+  console.log('Your address:', account);
+  accountElem.innerHTML = account;
+}
 
 ethereumButton.addEventListener('click', async () => {
   if (typeof window.ethereum !== 'undefined') {
     console.log('Metamask is installed!');
-  }
- 
-  if (typeof window.ethereum !== 'undefined') {
-    try {
-      window.web3 = web3
-
-      // backend should generate a random id and write it to the html, the js should read the value that the server wrote in the html (by reading the value in window.sessionID)
-      //crypto.randomUUID()
-      //'13600d93-7c77-44c0-b11a-e7f7f1cb8cd4'
-
-      const dataToSignRaw = "AUTH SESSION ID 12312412312"  // should be changed to this value window.sessionID
-      const dataToSign = web3.utils.sha3(dataToSignRaw)
-      const signedAuthMessage = await web3.eth.sign(dataToSign, account);
-      console.log('Autorization in MetaMask:', signedAuthMessage);
-
-      // send signedAuthMessage to the server with an ajax request axios (https://axios-http.com/docs/post_example)
-
-      // this needs to be moved on nodejs on the server
-      // this line needs to run as
-      const recovered = await web3.eth.accounts.recover(dataToSign, signedAuthMessage, true);
-      console.log('recovered:', recovered);
-
-
-    } catch (error) {
-      console.error(error);
-    }
+    await connectMetamask();
   } else {
-    console.log('MetaMask not find');
+    console.log('MetaMask not found');
   }
 });
